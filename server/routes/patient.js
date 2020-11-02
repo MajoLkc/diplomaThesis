647b74431edcae0a21654f8b61cdbe61e934a855
firemnyOscar <firemnyoscar@gmail.com>
2020-11-02 18:03:09 +0100

    added function checking if patient exists in DB

--- server/routes/patient.js
+++ server/routes/patient.js
@@ -25,3 +25,18 @@ export function handleCreatePatient(req, res) {
     res.json(data);
   });
 }
+
+/* This function isPatientPresent, we need to add to function handleCreatePatient
+if patient is present -> redirect to patient update page or add new informations about patient to DB
+*/
+
+export function isPatientPresent(req, res) {
+  const data = req.body;
+  db.collection('patients').findOne({ 'name': data.name, 'surname': data.surname, 'birthDate': data.birthDate }, (error, exist) => {
+    if (exist && !error) {
+      console.log(exist.informations[3])
+
+      res.jsonp({ success: true, patientRegistered: exist.informations[3] })
+    }
+  })
+}
