2762a38cbbc8eef7ee3ba5f7b30dedce94ea3f44
firemnyOscar <firemnyoscar@gmail.com>
2020-11-03 17:35:05 +0100

    another one I forgot (same as 2 previous)

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -1,5 +1,4 @@
 const MENU_VIEW = 'menu';
-const FORM_SELECTION_VIEW = 'formSelection';
 
 export function menu(req, res) {
   if (req.user) {
@@ -8,15 +7,5 @@ export function menu(req, res) {
       noLogout: false,
     };
     res.render(MENU_VIEW, context);
-  }
-}
-
-export function formSelection(req, res) {
-  if (req.user) {
-    const context = {
-      pageTitle: 'Výber dotazníka',
-      noLogout: false
-    };
-    res.render(FORM_SELECTION_VIEW, context);
   }
 }
