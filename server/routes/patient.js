4146bf189ff548c4bf7dc2094b049a39a794939b
firemnyOscar <firemnyoscar@gmail.com>
2020-11-03 17:37:39 +0100

    Added validation if patient is already inside DB

--- server/routes/patient.js
+++ server/routes/patient.js
@@ -15,22 +15,46 @@ export function newPatient(req, res) {
 
 export function handleCreatePatient(req, res) {
   const data = req.body; //ulozi data z formularu do premennej data
-  data.id = uuidv4();
-  db.collection('patients').insertOne(data, (err) => {
-    if (err) {
-      console.log(err);
-      res.sendStatus(500);
-      return; //aby to nepokracovalo
+  const patientDetails = data.informations;
+  const checkData = {
+    name: data.name,
+    surname: data.surname,
+    birthDate: data.birthDate
+  };
+  db.collection('patients').findOne(checkData, (error, exist) => {
+    //Patient is present inside DB
+    if (exist && !error) {
+      db.collection('patients').findOneAndUpdate(checkData, { $push: { informations: data.informations } }, (err, doc) => {
+        if (err) {
+          console.log(err);
+          res.sendStatus(500);
+          return
+        }
+        data.id = doc.value.id;
+        res.json(data);
+      });
+      //Patient is not present in DB yet
+    } else if (!exist) {
+      data.informations = [];
+      data.informations[0] = patientDetails;
+      data.id = uuidv4();
+      db.collection('patients').insertOne(data, (err) => {
+        if (err) {
+          console.log(err);
+          res.sendStatus(500);
+          return; //aby to nepokracovalo
+        }
+        res.json(data);
+      });
     }
-    res.json(data);
-  });
+  })
 }
 
 /* This function isPatientPresent, we need to add to function handleCreatePatient
 if patient is present -> redirect to patient update page or add new informations about patient to DB
 */
 
-export function isPatientPresent(req, res) {
+/*export */function isPatientPresent(req, res) {
   const data = req.body;
   db.collection('patients').findOne({ 'name': data.name, 'surname': data.surname, 'birthDate': data.birthDate }, (error, exist) => {
     if (exist && !error) {
