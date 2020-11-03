96d521b4ebd14d6dd2771c485942f6ff327f41f1
firemnyOscar <firemnyoscar@gmail.com>
2020-11-03 21:36:27 +0100

    Added new handling of MQ form

--- server/routes/MQ.js
+++ server/routes/MQ.js
@@ -13,16 +13,16 @@ export function morningQuestionaire(req, res) {
 }
 
 export function handleMorningQuestionaire(req, res) {
-  const updatedPatient = req.body;
+  const formValues = req.body;
   const url = req.originalUrl;
   const param = url.split('=');
   const data = { id: param[1] };
-  db.collection('patients').findOneAndUpdate(data, { $set: updatedPatient }, (err) => {
+  db.collection('patients').findOneAndUpdate(data, { $push: { MqForm: formValues } }, (err, doc) => {
     if (err) {
       console.log(err);
       res.sendStatus(500);
       return;
     }
     res.json(data);
-  });
+  })
 }
