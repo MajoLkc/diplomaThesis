7c80ece424f3649d9934388594fc60b59abb1962
firemnyOscar <firemnyoscar@gmail.com>
2020-11-04 19:45:40 +0100

    Update disable btns if form is not present in db

--- server/routes/showQuestionaire.js
+++ server/routes/showQuestionaire.js
@@ -1,32 +1,33 @@
 import { db } from '../db.js';
 
 const SHOW_QUESTIONAIRE_VIEW = 'showQuestionaire';
-let noCSQ;
-let noOSAS;
-let noESS;
-let noMQ;
+
 
 export function showQuestionaire(req, res) {
   if (req.user) {
     const url = req.originalUrl;
     const param = url.split('=');
     const data = { id: param[1] };
+    let csq;
+    let osas;
+    let ess;
+    let mq;
     db.collection('patients').findOne(data, (err, doc) => {
       if (err) throw err;
-      noCSQ = null;
-      noOSAS = null;
-      noESS = null;
-      noMQ = null;
-      if (doc.CSQ_creationDate === undefined) noCSQ = 'disabled';
-      if (doc.OSAS_creationDate === undefined) noOSAS = 'disabled';
-      if (doc.ESS_creationDate === undefined) noESS = 'disabled';
-      if (doc.MQ_creationDate === undefined) noMQ = 'disabled';
+      csq = null;
+      osas = null;
+      ess = null;
+      mq = null;
+      if (!doc.CsqForm.length) csq = 'disabled';
+      if (!doc.OsasForm.length) osas = 'disabled';
+      if (!doc.EssForm.length) ess = 'disabled';
+      if (!doc.MqForm.length) mq = 'disabled';
       const context = {
         pageTitle: 'Zoznam dotazníkov pacienta',
-        CSQ: noCSQ,
-        OSAS: noOSAS,
-        ESS: noESS,
-        MQ: noMQ,
+        csqDisabled: csq,
+        osasDisabled: osas,
+        essDisabled: ess,
+        mqDisabled: mq,
         noLogout: false
       };
       res.render(SHOW_QUESTIONAIRE_VIEW, context);
