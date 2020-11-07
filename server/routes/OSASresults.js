ea9f2429e79d6f79b0a98bd93cf42cb5ec023a0d
firemnyOscar <firemnyoscar@gmail.com>
2020-11-07 16:27:33 +0100

    Updated OSAS form result page (new multiple forms)

--- server/routes/OSASresults.js
+++ server/routes/OSASresults.js
@@ -2,38 +2,41 @@ import { db } from '../db.js';
 
 const OSAS_RESULTS_VIEW = 'OSASresults';
 
-function splitStr(str) {
-  // Function to split string
-  return str.split('=');
+function splitStrAndGetValue(str) {
+  return str.split('=')[1];
 }
 
 export function OSASresults(req, res) {
   if (req.user) {
     const url = req.originalUrl;
-    const id = splitStr(url);
-    const data = { id: id[1] };
+    const patientUrl = url.split('&')[0];
+    const formUrl = url.split('&')[1];
+    const patientId = splitStrAndGetValue(patientUrl);
+    const formId = splitStrAndGetValue(formUrl);
+    const data = { id: patientId };
     db.collection('patients').findOne(data, (err, doc) => {
+      if (err) throw err;
       const context = {
         patientName: doc.name,
         patientSurname: doc.surname,
-        creationDate: doc.OSAS_creationDate,
-        creator: doc.OSAS_creator,
-        answer1: doc.OSAS_answer_1,
-        answer2: doc.OSAS_answer_2,
-        answer3: doc.OSAS_answer_3,
-        answer4: doc.OSAS_answer_4,
-        answer5: doc.OSAS_answer_5,
-        answer6: doc.OSAS_answer_6,
-        answer7: doc.OSAS_answer_7,
-        answer8: doc.OSAS_answer_8,
-        answer9: doc.OSAS_answer_9,
-        answer10: doc.OSAS_answer_10,
-        answer11: doc.OSAS_answer_11,
-        answer12: doc.OSAS_answer_12,
-        answer13: doc.OSAS_answer_13,
-        answer14: doc.OSAS_answer_14,
-        answer15: doc.OSAS_answer_15,
-        answer16: doc.OSAS_answer_16,
+        creationDate: doc.OsasForm[formId][17],
+        creator: doc.OsasForm[formId][0],
+        answer1: doc.OsasForm[formId][1],
+        answer2: doc.OsasForm[formId][2],
+        answer3: doc.OsasForm[formId][3],
+        answer4: doc.OsasForm[formId][4],
+        answer5: doc.OsasForm[formId][5],
+        answer6: doc.OsasForm[formId][6],
+        answer7: doc.OsasForm[formId][7],
+        answer8: doc.OsasForm[formId][8],
+        answer9: doc.OsasForm[formId][9],
+        answer10: doc.OsasForm[formId][10],
+        answer11: doc.OsasForm[formId][11],
+        answer12: doc.OsasForm[formId][12],
+        answer13: doc.OsasForm[formId][13],
+        answer14: doc.OsasForm[formId][14],
+        answer15: doc.OsasForm[formId][15],
+        answer16: doc.OsasForm[formId][16],
         pageTitle: 'Syndróm OSA',
         noLogout: false
       };
