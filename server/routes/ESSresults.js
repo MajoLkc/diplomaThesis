3dcdbaa141326b002c4b641cf5e27a67306611b7
firemnyOscar <firemnyoscar@gmail.com>
2020-11-07 16:18:22 +0100

    Updated displaying of ESS form results

--- server/routes/ESSresults.js
+++ server/routes/ESSresults.js
@@ -2,32 +2,36 @@ import { db } from '../db.js';
 
 const ESS_RESULTS_VIEW = 'ESSresults';
 
-function splitStr(str) {
+function splitStrAndGetValue(str) {
   // Function to split string
-  return str.split('=');
+  return str.split('=')[1];
 }
 
 export function ESSresults(req, res) {
   if (req.user) {
     const url = req.originalUrl;
-    const id = splitStr(url);
-    const data = { id: id[1] };
+    const patientUrl = url.split('&')[0];
+    const formUrl = url.split('&')[1];
+    const patientId = splitStrAndGetValue(patientUrl);
+    const formId = splitStrAndGetValue(formUrl);
+    const data = { id: patientId };
     db.collection('patients').findOne(data, (err, doc) => {
+      if (err) throw err;
+      const sum = Number(doc.EssForm[formId][1]) + Number(doc.EssForm[formId][2]) + Number(doc.EssForm[formId][3]) + Number(doc.EssForm[formId][4]) + Number(doc.EssForm[formId][5]) + Number(doc.EssForm[formId][6]) + Number(doc.EssForm[formId][7]) + Number(doc.EssForm[formId][8]);
       const context = {
         patientName: doc.name,
         patientSurname: doc.surname,
-        creationDate: doc.ESS_creationDate,
-        creator: doc.ESS_creator,
-        answer1: doc.ESS_answer_1,
-        answer2: doc.ESS_answer_2,
-        answer3: doc.ESS_answer_3,
-        answer4: doc.ESS_answer_4,
-        answer5: doc.ESS_answer_5,
-        answer6: doc.ESS_answer_6,
-        answer7: doc.ESS_answer_7,
-        answer8: doc.ESS_answer_8,
-        // eslint-disable-next-line max-len
-        result: Number(doc.ESS_answer_1) + Number(doc.ESS_answer_2) + Number(doc.ESS_answer_3) + Number(doc.ESS_answer_4) + Number(doc.ESS_answer_5) + Number(doc.ESS_answer_6) + Number(doc.ESS_answer_7) + Number(doc.ESS_answer_8),
+        creationDate: doc.EssForm[formId][9],
+        creator: doc.EssForm[formId][0],
+        answer1: doc.EssForm[formId][1],
+        answer2: doc.EssForm[formId][2],
+        answer3: doc.EssForm[formId][3],
+        answer4: doc.EssForm[formId][4],
+        answer5: doc.EssForm[formId][5],
+        answer6: doc.EssForm[formId][6],
+        answer7: doc.EssForm[formId][7],
+        answer8: doc.EssForm[formId][8],
+        result: sum,
         pageTitle: 'Epworthská škála spavosti',
         noLogout: false
       };
